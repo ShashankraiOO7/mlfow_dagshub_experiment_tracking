ad50f6b26608f2791e3d567f6fa0b2b5c14fde50
ShashankraiOO7 <shashankrai020700@gmail.com>
2025-02-13 17:59:35 +0530

    updation model

--- model_build.py
+++ model_build.py
@@ -6,9 +6,15 @@ import seaborn as sns
 import mlflow
 import matplotlib.pyplot as plt
 import dagshub
+
+# Initialize DAGsHub integration
 dagshub.init(repo_owner='ShashankraiOO7', repo_name='mlfow_dagshub_experiment_tracking', mlflow=True)
 
-mlflow.set_tracking_uri('https://github.com/ShashankraiOO7/mlfow_dagshub_experiment_tracking.git')
+# Set MLflow tracking URI to DAGsHub (not GitHub!)
+mlflow.set_tracking_uri('https://dagshub.com/ShashankraiOO7/mlfow_dagshub_experiment_tracking.mlflow')
+
+# Set MLflow experiment
+mlflow.set_experiment('new_one')
 
 # Load the dataset
 df = pd.read_csv(r'./data/classification_data_5000_records.csv')
@@ -24,10 +30,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 n_estimators = 100
 max_depth = 7
 
-# Initialize the Random Forest classifier
+# Initialize and train the Random Forest classifier
 clf = RandomForestClassifier(random_state=42, n_estimators=n_estimators, max_depth=max_depth)
-
-# Train the model
 clf.fit(X_train, y_train)
 
 # Make predictions
@@ -36,16 +40,12 @@ y_pred = clf.predict(X_test)
 # Evaluate the model
 accuracy = accuracy_score(y_test, y_pred)
 
-# Set MLflow experiment
-mlflow.set_experiment('new one')
-
 # Calculate confusion matrix
 cm = confusion_matrix(y_test, y_pred)
 
 # Plot confusion matrix
 plt.figure(figsize=(6, 6))
-sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', 
-            xticklabels=y.unique(), yticklabels=y.unique())
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=y.unique(), yticklabels=y.unique())
 plt.ylabel('Actual')
 plt.xlabel('Predicted')
 plt.title('Confusion Matrix')
@@ -64,3 +64,11 @@ with mlflow.start_run(run_name='444'):
     mlflow.log_param('n_estimators', n_estimators)
     mlflow.log_param('max_depth', max_depth)
     mlflow.log_artifact("confusion_matrix.png")
+    mlflow.log_artifact(__file__)
+
+    mlflow.sklearn.log_model(clf, "Random Forest")
+
+    mlflow.set_tag('author','Shashank')
+    mlflow.set_tag('model','Random Forest')
+
+    print('accuracy', accuracy)
